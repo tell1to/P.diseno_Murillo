061e0ad22148aa29559f37fc98592b0aa5b8a2d5
tell1to <hmd.murillo@yavirac.edu.ec>
2024-12-16 02:19:07 -0500

    Avance_Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { Grid1Component } from './components/grid-1/grid-1.component';
 import { Grid2Component } from './components/grid-2/grid-2.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FormsNGComponent } from './components/forms-ng/forms-ng.component';
+
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { HomeComponent } from './components/home/home.component';
 import { AvatarModule } from 'primeng/avatar';
 import { ToolbarModule } from 'primeng/toolbar';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { FormsNGComponent } from './components/forms-ng/forms-ng.component';
-
  import { FormsModule } from '@angular/forms';
  import { CheckboxModule } from 'primeng/checkbox'
  import { InputTextModule } from 'primeng/inputtext';
+ import { ColorPickerModule } from 'primeng/colorpicker';
+ import { AnimateModule } from 'primeng/animate';
+ import { ContextMenuModule } from 'primeng/contextmenu';
+  import { DataViewModule } from 'primeng/dataview';
+  import { VirtualScrollerModule } from 'primeng/virtualscroller';
 
 
 
@@ -40,7 +46,12 @@ import { FormsNGComponent } from './components/forms-ng/forms-ng.component';
     AvatarModule,
     FormsModule,
     InputTextModule,
-    CheckboxModule
+    CheckboxModule,
+    ColorPickerModule,
+    AnimateModule,
+    ContextMenuModule, 
+    DataViewModule,
+    VirtualScrollerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/forms-ng/forms-ng.component.ts
+++ src/app/components/forms-ng/forms-ng.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'primeng/colorpicker';
 
 @Component({
   selector: 'app-forms-ng',
   standalone: false,
   
   templateUrl: './forms-ng.component.html',
-  styleUrl: './forms-ng.component.css'
+  styleUrl: './forms-ng.component.css',
+  
+  
 })
 export class FormsNGComponent {
   user: any ={};
   acceptTerms: boolean = false; // Agrega esta propiedad
+  favoriteColor: string = '';
+  color: string | undefined;
   submit(){
-    console.log('Formulario enviado', this.user)
+    console.log('Formulario enviado', this.user, this.acceptTerms, this.favoriteColor);
   }
 }
